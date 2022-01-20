4fd91c55ab972d8c116246f016cd94f7506cf01b
nandaasli <97029018+nandaasli@users.noreply.github.com>
2022-01-20 23:53:37 +0700

    Update app_uas.py

--- app_uas.py
+++ app_uas.py
@@ -10,15 +10,7 @@ def index():
 return render_template('index backup uas.html', hasil=0, nhasil=0)
 @app.route('/predict', methods=['POST'])
 def predict():
-ntahun = request.form.get('ntahun')
-data=('Unnamed','dissimilarity','correlation','homogeneity','contrast','ASM','energy','label','label2')
-nstart=len(data)
-nseries=int(ntahun)-1
-nakhir=nstart+nseries
-prediction = model.predict(start=nstart, end=nakhir, dynamic=False)
-output = np.round(prediction, 2)
-nhasil = len(output)
-return render_template('index backup uas.html', hasil=output, ntahun=nakhir, nhasil=nhasil)
+
 
 if __name__ == '__main__':
 app.run(debug=True)
